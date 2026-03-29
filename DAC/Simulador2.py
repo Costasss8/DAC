96f417b4882538ca245918b77a78434fe4c7e393
Costasss8 <a26700@alunos.aepbs.net>
2026-03-29 22:28:06 +0100

    Delete DAC directory

--- DAC/Simulador2.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def criaSimulador2():
-    return ft.Column(
-        [
-            ft.Text("Simulador 2", size=22, weight=ft.FontWeight.BOLD),
-        ],
-        expand=True,
-    )
